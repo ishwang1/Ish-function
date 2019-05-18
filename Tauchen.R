2b7cccac04c18027d89e0b99815beaf3cf3cf76c
Yishu Wang <wangy.ish@outlook.com>
2019-05-19 02:54:24 +0800

    Update Tauchen.R

--- Tauchen.R
+++ Tauchen.R
@@ -10,7 +10,7 @@ Tauchen=function(epsilon_cond,n=9,m=3,rho=0,alpha=0,sigma=1) {
   w=(kn-k1)/(n-1)/2
   
   p1=pnorm(k1+w,mean=mean_cond,sd=sigma)
-  p=pnorm(epsilon[2:(n-1)]+w,,mean=mean_cond,sd=sigma)-pnorm(epsilon[2:(n-1)]-w,,mean=mean_cond,sd=sigma)
+  p=pnorm(epsilon[2:(n-1)]+w,mean=mean_cond,sd=sigma)-pnorm(epsilon[2:(n-1)]-w,mean=mean_cond,sd=sigma)
   pn=1-pnorm(kn-w,mean=mean_cond,sd=sigma)
   
   return(data.frame(epsilon=epsilon,CondProb=c(p1,p,pn)))
