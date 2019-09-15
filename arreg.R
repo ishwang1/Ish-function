03c828a824863a18ecec671fbc3007d22330867a
Yishu Wang <wangy.ish@outlook.com>
2019-09-16 00:23:51 +0800

    update arreg with 1-step forecast

--- arreg.R
+++ arreg.R
@@ -8,8 +8,6 @@ arreg=function(y,order.max=10,HQ.c=2) {
     LY=cbind(LY,c(rep(NA,p),y[-((n-p+1):n)]))
   }
   
-  
-  
   arreg.order=function(p) {
     if(p>0) {
       y_tilde=y[(p+1):n]
@@ -21,19 +19,20 @@ arreg=function(y,order.max=10,HQ.c=2) {
         AIC=log(sigma2)+2*p/(n-p)
         BIC=log(sigma2)+p*log(n-p)/(n-p)
         HQIC=log(sigma2)+HQ.c*p*log(log(n-p))/(n-p)
-        return(c(alpha,rep(NA,order.max-p),sigma2,AIC,BIC,HQIC))
+        forecast=sum(rev(y[(n-p+1):n])*alpha)
+        return(c(alpha,rep(NA,order.max-p),sigma2,AIC,BIC,HQIC,forecast))
       } else {
-        return(rep(NA,order.max+3))
+        return(rep(NA,order.max+5))
       }
     } else {
       sigma2=mean(y^2)
-      return(c(rep(NA,order.max),sigma2,rep(log(sigma2),3)))
+      return(c(rep(NA,order.max),sigma2,rep(log(sigma2),3),0))
     }
   }
   
   result=foreach(p=0:order.max,.combine=rbind) %do% {arreg.order(p)}
   rownames(result)=paste("AR(",0:order.max,")",sep="")
-  colnames(result)=c(paste("a",1:order.max,sep=""),"sigma2","AIC","BIC","HQIC")
+  colnames(result)=c(paste("a",1:order.max,sep=""),"s^2","AIC","BIC","HQIC","forecast")
   
   return(result)
 }
